2222c417b7c24d802532f45dba9ffa7759906fbb
donlangdang <donlangdangcc@gmail.com>
2024-06-02 22:07:48 +0700

    fix xong bug ko register dc vi ko dung dc or va nen dung in [] python coi chuoi rong la True

--- Users/views.py
+++ Users/views.py
@@ -15,9 +15,10 @@ from rest_framework.permissions import AllowAny
 # Create your views here.
 class UserRegisterAndGetUserById(APIView):
   
+  
   # tùy chỉnh permissions với phương thức get hoặc post trong class base
   def get_permissions(self):
-    if self.request.method == 'GET' or 'PUT':
+    if self.request.method in ['GET', 'PUT']:
       return [PermissionsUserId()]
     elif self.request.method == 'POST':
       return [AllowAny()]
@@ -25,39 +26,36 @@ class UserRegisterAndGetUserById(APIView):
   
   # đăng kí người dùng mới
   def post(self, request):
-    if request.method == 'POST':
-      dataSerializers = GetAllUsers(data=request.data)
-      if dataSerializers.is_valid():
-        dataSerializers.save()
-        return Response(dataSerializers.data, status=status.HTTP_201_CREATED)
-      return Response(dataSerializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    dataSerializers = GetAllUsers(data=request.data)
+    if dataSerializers.is_valid():
+      dataSerializers.save()
+      return Response(dataSerializers.data, status=status.HTTP_201_CREATED)
+    return Response(dataSerializers.errors, status=status.HTTP_400_BAD_REQUEST)
     
   # lấy data người dùng theo permissions
   # những ai có access token mới xem đc cái này
   # đại khái như ai đăng nhập thì coi đc á
   def get(self, request, pk):
-    if request.method == 'GET':
-      try:
-        modelUserById = CustomUser.objects.get(pk=pk)
-      except:
-        return Response({'mess': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
-      getUserById = GetAllUsers(modelUserById)
-      if not getUserById:
-        return Response(getUserById.errors, status=status.HTTP_400_BAD_REQUEST)
-      return Response(getUserById.data, status=status.HTTP_200_OK)
+    try:
+      modelUserById = CustomUser.objects.get(pk=pk)
+    except:
+      return Response({'mess': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+    getUserById = GetAllUsers(modelUserById)
+    if not getUserById:
+      return Response(getUserById.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(getUserById.data, status=status.HTTP_200_OK)
     
   # cập nhật thông tin user    
   def put(self, request, pk):
-    if request.method == 'PUT':
-      user = CustomUser.objects.get(pk=pk)
-      # lấy object user với pk rồi gắn vô làm đối số với GetAllUser
-      # để nó biết được gắn vô gàm update bên đó.
-      # partial=True dùng cái này để có thể cập nhật 1 phần
-      dataUpdateUser = GetAllUsers(user, data=request.data, partial=True)
-      if dataUpdateUser.is_valid():
-        dataUpdateUser.save()
-        return Response(dataUpdateUser.data, status=status.HTTP_201_CREATED)
-      return Response(dataUpdateUser.errors, status=status.HTTP_400_BAD_REQUEST)
+    user = CustomUser.objects.get(pk=pk)
+    # lấy object user với pk rồi gắn vô làm đối số với GetAllUser
+    # để nó biết được gắn vô gàm update bên đó.
+    # partial=True dùng cái này để có thể cập nhật 1 phần
+    dataUpdateUser = GetAllUsers(user, data=request.data, partial=True)
+    if dataUpdateUser.is_valid():
+      dataUpdateUser.save()
+      return Response(dataUpdateUser.data, status=status.HTTP_201_CREATED)
+    return Response(dataUpdateUser.errors, status=status.HTTP_400_BAD_REQUEST)
       
       
 class GetAllUserAPI(APIView):
